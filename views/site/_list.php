a7ed35c71dccdcc17369b6c13113c4750f918143
Sergey Gumeniuk <blog-a@ukr.net>
2018-07-19 20:30:11 +0300

    Update _list.php

--- views/site/_list.php
+++ views/site/_list.php
@@ -1,20 +1,15 @@
 <?php
 use yii\helpers\Html;
-if (!empty($model->gravatar_id)) {
-  $avatar = $model->gravatar_id;
-}else {
-  $avatar = '/img/no-avatar.png';
-}
  ?>
 <div class="panel-body">
   <div class="col-md-1">
     <?php if (Yii::$app->user->can('adminAccess')): ?>
       <?php  echo  Html::a(
-        '<img src="'.$avatar.'" class="img-thumbnail" width="50" alt="Cinque Terre">',
+        '<img src="/img/no-avatar.png" class="img-thumbnail" width="50" alt="Cinque Terre">',
          ['user/profile/show', 'id' => $model->user_id]
        );?>
     <?php else: ?>
-      <img src="<?=$avatar?>" class="img-thumbnail" width="50" alt="Cinque Terre">
+      <img src="/img/no-avatar.png" class="img-thumbnail" width="50" alt="Cinque Terre">
     <?php endif; ?>
   </div>
   <div class="col-md-2">
